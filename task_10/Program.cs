50629f54088db0d9f049102db31df0b2d4982b3c
Lexuse <Lexuse2007@gmail.com>
2022-11-19 14:37:01 +0300

    task_10 finished

---
+++ task_10/Program.cs
@@ -0,0 +1,16 @@
+﻿//Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+int inputNumber; 
+
+while (true)
+{    
+    Console.WriteLine ("введите пожалуйста целое трехзначное чило");
+    if (int.TryParse(Console.ReadLine(), out inputNumber))
+        break;
+    Console.WriteLine("ошибка ввода! нужно вводить цифры");
+}
+
+int a = inputNumber / 10;
+int b = a % 10;
+
+Console.WriteLine ($"{inputNumber} -> {b}");
