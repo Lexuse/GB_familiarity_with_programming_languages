3439e8d14ae49779c2c209a2e5466c3162e6d6af
Lexuse <Lexuse2007@gmail.com>
2022-11-21 21:42:39 +0300

    task_19 finished

---
+++ task_19/Program.cs
@@ -0,0 +1,54 @@
+﻿//Программа которая принимает на вход пятизначное число и проверяет, является ли оно полиндромом
+
+Console.Clear();
+int userInput5Number = 0;
+int reverse = 0;
+
+
+    try
+    {
+        Console.WriteLine ("введите пятизначное число для проверки его на соответсвие палиндрому");
+        userInput5Number = int.Parse(Console.ReadLine() ?? ""); //сразу выполним проверку на Null
+    }
+    catch(Exception exc)
+    {
+        Console.WriteLine ($"Ошибка ввода данных! Ведите пятизначное число {exc.Message}");
+    }
+
+    reverse = Reverse5UserNumber(userInput5Number);
+
+    if (userInput5Number == reverse)
+            Console.WriteLine($"{userInput5Number} -> да");
+        else
+            Console.WriteLine($"{userInput5Number} -> нет");
+
+
+
+
+static int Reverse5UserNumber (int userInput5Number) //создадим метод, который переворачивает введенное число
+{
+int a; 
+int reverse = 0;      //просто переменная для хранения временного значения
+a = userInput5Number % 10;
+userInput5Number = userInput5Number / 10;
+reverse += a * 10000;
+
+a = userInput5Number % 10;
+userInput5Number = userInput5Number / 10;
+reverse += a * 1000;
+
+a = userInput5Number % 10;
+userInput5Number = userInput5Number / 10;
+reverse += a * 100;
+
+a = userInput5Number % 10;
+userInput5Number = userInput5Number / 10;
+reverse += a * 10;
+
+a = userInput5Number % 10;
+userInput5Number = userInput5Number / 10;
+reverse += a;
+
+//Console.WriteLine(reverse);
+return reverse;
+}
