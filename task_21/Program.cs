4eb0f3400c88bb06049b4ee0dc3c6cacf97fbc08
Lexuse <Lexuse2007@gmail.com>
2022-11-22 11:27:08 +0300

    task_21 finished

---
+++ task_21/Program.cs
@@ -0,0 +1,29 @@
+﻿// Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Clear();
+double distance;
+
+double x1 = UserInput("Введите координату x1: ");
+double y1 = UserInput("Введите координату y1: ");
+double z1 = UserInput("Введите координату z1: ");
+
+double x2 = UserInput("Введите координату x2: ");
+double y2 = UserInput("Введите координату y2: ");
+double z2 = UserInput("Введите координату z2: ");
+
+distance = DisPoint(x1, y1, z1, x2, y2, z2);
+
+Console.WriteLine($"A({x1},{y1},{z1}); B({x2},{y2},{z2}), -> {distance}");
+
+static double DisPoint(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+}
+
+
+//метод, принимающий на вход просьбу вести заданную координату и возвращающий её double значение
+static double UserInput (string message)
+{
+    Console.Write(message);
+    return double.Parse(Console.ReadLine() ?? "");
+}
