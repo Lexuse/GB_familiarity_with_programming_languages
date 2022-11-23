2ea65878500578c63e12a457f4a09262556d08e6
Lexuse <Lexuse2007@gmail.com>
2022-11-23 12:02:12 +0300

    task_27 finished

---
+++ task_27/Program.cs
@@ -0,0 +1,31 @@
+﻿// Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+int number = userInputNumber("Введите число  ","Ошибка ввода!  ");
+int res =0;
+
+
+
+while (number > 0)
+{
+   res += number % 10;
+   number /= 10;
+}
+
+
+Console.Write($"{res}");
+
+
+////////////////////////////////////////////////////////
+int userInputNumber (string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+
+        if (int.TryParse(Console.ReadLine(), out int userInpNumb))
+            return userInpNumb;
+        
+        Console.WriteLine(errorMessage);
+    }
+}
