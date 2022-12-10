7aed7844984e413d31d89bafbd488d6b1bf6e0e8
Lexuse <Lexuse2007@gmail.com>
2022-12-10 13:21:28 +0300

    task_53 finished

---
+++ task_53/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задайте двумерный массив. Напишите программу, которая меняет местами первую и последнюю строку массива
+
+Console.Clear();
+
+int[,] array = GenerateArray(5,5);
+PrintArray(array);
+Console.WriteLine("#############");
+ChangeArrayRows(array);
+PrintArray(array);
+
+////////////////////////////////////////
+int[,] GenerateArray(int row, int col)
+{
+    int[,] arr = new int[row,col];
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            arr[i,j] = new Random().Next(1,9);
+        }    
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeArrayRows(int[,] arr)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {   
+        int temp = arr[0,j]; //сохранили значение 1го элемента в первой строке
+        arr[0,j] = arr[arr.GetLength(0) -1, j]; //заменили значение первого элемента в строке значением первого элемента в последней строке
+        arr[arr.GetLength(0) - 1, j] = temp; // присволили значение первого элемента в первой строке первому элементу в последней строке, которое ранее было сохранено в temp
+    }
+}
