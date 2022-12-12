403209d148cfba9131de0abb5d16edcb952a2e60
Lexuse <Lexuse2007@gmail.com>
2022-12-12 12:47:26 +0300

    task_60 finished

---
+++ task_60/Program.cs
@@ -0,0 +1,82 @@
+﻿//.Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+Console.Clear();
+
+Console.WriteLine("Для создания трехмерного массива, введите по очереди числа");
+
+int x = GetInputUserNumber("введите число X ", "Ошибка ввода");
+int y = GetInputUserNumber("введите число Y ", "Ошибка ввода");
+int z = GetInputUserNumber("введите число Z ", "Ошибка ввода");
+
+
+int[,,] array3D = new int[x, y, z];
+CreateArray(array3D);
+PrintArray(array3D);
+
+////////////////////////////////////////////////////////////////
+
+int GetInputUserNumber(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.WriteLine($"{message}");
+        if (int.TryParse(Console.ReadLine(), out int mumber))
+            return mumber;
+        Console.WriteLine($"{errorMessage}");
+    }
+}
+
+void CreateArray(int[,,] array3D)
+{
+  int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int x = 0; x < array3D.GetLength(0); x++)
+  {
+    for (int y = 0; y < array3D.GetLength(1); y++)
+    {
+      for (int z = 0; z < array3D.GetLength(2); z++)
+      {
+        array3D[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
+}
+
+void PrintArray (int[,,] array3D)
+{
+  for (int i = 0; i < array3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+      Console.Write($"X({i}) Y({j}) ");
+      for (int k = 0; k < array3D.GetLength(2); k++)
+      {
+        Console.Write( $"Z({k})={array3D[i,j,k]}; ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
+
