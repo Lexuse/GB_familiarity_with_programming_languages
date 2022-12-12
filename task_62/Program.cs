5c22039326cee2b80f5726c374fa037472691408
Lexuse <Lexuse2007@gmail.com>
2022-12-12 13:14:59 +0300

    task_62 finished

---
+++ task_62/Program.cs
@@ -0,0 +1,61 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+
+
+
+int[,] array = GenerateArray(4, 4, 1, 10);
+
+array = FillArray(array);
+
+PrintArray(array);
+
+
+
+///////////////////////////////////////////////////////////////
+int[,] GenerateArray(int row, int col, int minValue, int maxValue)
+{
+    int[,] arr = new int[row, col];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return arr;
+}
+
+int[,] FillArray(int[,] arr)
+{
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+
+    while (temp <= arr.GetLength(0) * arr.GetLength(1))
+    {
+        arr[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < arr.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= arr.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > arr.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
